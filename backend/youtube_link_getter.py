863e97752511e4b1d1cca5407cbf2408688ec39d
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-11-04 00:49:42 +0000

    youtubelinks guaranteed under 5.5 minutes

--- backend/youtube_link_getter.py
+++ backend/youtube_link_getter.py
@@ -1,15 +1,13 @@
-
 import os
+import isodate  # Install this library using pip
 from dotenv import load_dotenv
 import requests
-load_dotenv()
 
+load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
-
 def get_youtube_link(search_query):
-    # Perform the search
-
+    # Step 1: Perform the search
     response = requests.get(
         'https://www.googleapis.com/youtube/v3/search',
         params={
@@ -17,10 +15,28 @@ def get_youtube_link(search_query):
             'q': search_query,
             'type': 'video',
             'part': 'snippet',
-            'maxResults': 1
+            'maxResults': 10  # Fetch more results in case the first few videos are longer than 5.5 minutes
         }
     )
-    video_id = response.json()['items'][0]['id']['videoId']
-    video_url = f'https://www.youtube.com/watch?v={video_id}' 
-    return video_url  
+    
+    video_ids = [item['id']['videoId'] for item in response.json()['items']]
 
+    # Step 2: Retrieve video details
+    response = requests.get(
+        'https://www.googleapis.com/youtube/v3/videos',
+        params={
+            'key': GOOGLE_API_KEY,
+            'id': ','.join(video_ids),
+            'part': 'contentDetails'
+        }
+    )
+
+    for item in response.json()['items']:
+        duration = isodate.parse_duration(item['contentDetails']['duration'])
+        if duration.total_seconds() <= 5.5 * 60:  # Check if duration is less than or equal to 5.5 minutes
+            return f"https://www.youtube.com/watch?v={item['id']}", duration.total_seconds()/60  # Return the video link and duration in minutes
+    
+    return None  # Return None if no suitable video found
+
+# # Test
+# print(get_youtube_link("how to make a cake"))
