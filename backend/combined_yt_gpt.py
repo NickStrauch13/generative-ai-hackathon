e42ad34675a19688a2a3a7e241ec1e88773d6b85
Mike <michaelkeohane01@gmail.com>
2023-11-03 22:51:54 -0400

    Testing pipeline

--- backend/combined_yt_gpt.py
+++ backend/combined_yt_gpt.py
@@ -8,11 +8,12 @@ def main_combined(query="My toilet is broken, what should I do?"):
     # get the transcript
     transcript = getYoutubeTranscript(youtube_link)
     # get the response
-    new_query = f"Based on this expert information '{transcript}', answer this question '{query}'"
-    response = query_gpt(query, transcript="transcript")
-    return response
+    new_query = f"Using this video transcript to help source information '{transcript}', answer this question '{query}'"
+    response = query_gpt(query, transcript=transcript, max_tokens=150)
+    return response, youtube_link[0]
 
 if __name__ == '__main__':
-    query = "How do I replace my garbage disposal?"
-    response = main_combined(query)
+    query = "how do I create my own dartboard?"
+    response, yt_link = main_combined(query)
     print(response)
+    print(yt_link)
