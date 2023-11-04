4a347d84101cd2aa16f4c94c868469a4d270d1fb
Nick Strauch <njs35@duke.edu>
2023-11-04 19:41:39 -0400

    added elaboration

--- frontend/src/apis/elaborate.js
+++ frontend/src/apis/elaborate.js
@@ -2,7 +2,7 @@ const handleElaborate = async (textValue, setElaborate, setHasElaborated) => {
     const elaborate_url = 'http://127.0.0.1:5000/elaborate_step'; 
 
     const data = {
-        query: textValue, 
+        step: textValue, 
     };
 
     try {
